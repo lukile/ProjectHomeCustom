2680c6d965050981d3afb0f722cde297d5fcfcde
Lukile <lucile.1988.ls@gmail.com>
2020-01-29 21:59:52 +0100

    Fix bug on all state check and added zone check

--- src/dialogflow-fulfillment/src/index.ts
+++ src/dialogflow-fulfillment/src/index.ts
@@ -20,9 +20,6 @@ assistant.intent('projecthomecustom.smarthome.device.state.check', async (conv, 
     let _devices: Device[];
     let _room: Room;
     let _openhabItem: Item;
-    let _state: string;
-
-    let responseMessage: string = `State : ${_state}`;
 
     if (!all) {
         await openhabClient.getAllDevices()
@@ -31,18 +28,16 @@ assistant.intent('projecthomecustom.smarthome.device.state.check', async (conv, 
         await openhabClient.getAllRooms()
             .then(x => _room = x.find(r => r.name === room));
 
-        // respond with appropriate sentense if device is not found
         if (!_device)
             conv.close(`Sorry, couldn't find any device of type ${deviceType} in ${_room.description}`);
 
-        // here device is found, get the state
         await openhabClient.getItem(_device.id)
             .then(x => _openhabItem = x);
-        // transform the state
         let state = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
-        // close conversation
         conv.close(`${deviceType} is ${state} in ${_room.description}`);
     } else {
+        let responseMessage: string = "State : \n";
+
         await openhabClient.getAllDevices()
             .then(x => _devices = x.filter(d => d.type == deviceType));
 
@@ -53,81 +48,84 @@ assistant.intent('projecthomecustom.smarthome.device.state.check', async (conv, 
 
         await Promise.all(_devices.map(async _device => {
             let _openhabItem: Item;
-            let initialState: String;
-            let bidon: String = "OPEN";
-            // get the initial state
+            let itemState: string;
+
             await openhabClient.getItem(_device.id)
                 .then(x => {
                     _openhabItem = x;
-                    initialState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
-                    console.log("Coucouuuuu");
+                    itemState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
+                    responseMessage = responseMessage.concat(`${_device.room} => ${_openhabItem.state} \n`);
                 });
-            //(async _ => {
-            // re-fetch updated state
-            console.log("getItem");
-            await openhabClient.getItem(_device.id).then(x => x.state)
-                .then(resp => {
-                    //x => _devices = x.filter(d => d.type == deviceType);
-                    console.log(`deviceType: ${initialState}`);
-                    responseMessage.concat(`=> ${bidon} has benn updated from ${initialState}  \n`);
-                    //conv.close(`Sorry, couldn't find any device of type ${deviceType}`);
-                });
-            conv.close(`Sorry, couldn't find any device of type ${deviceType}`);
-            //})
-            ;
-
-            // send command |or| update state
-
-            //responseMessage.concat(`=> ${deviceType} has benn updated from ${initialState} to ${updatedState}  \n`);
-            //responseMessage = responseMessage.concat(`=> ${deviceType} has benn updated from ${initialState} to ${updatedState}  \n`);
         }));
-
-        // conv.close(`${_openhabItem.label} is ${state} in ${_device.room}`);
-        //responseMessage.concat(`=> ${deviceType} has benn updated from ${_state} \n`);
-        //responseMessage = responseMessage.concat(`=> ${deviceType} has benn updated from ${_state}  \n`);
-
-
+        conv.ask(new SimpleResponse({
+            text: responseMessage,
+            speech: 'Are you talking to me ?'
+        }));
     }
 });
-
-
 
 assistant.intent('projecthomecustom.smarthome.device.command', async (conv, { room, deviceType, command, value }) => {
     let _device: Device;
     let _room: Room;
     let _openhabItem: Item;
 
-    // fetch configurations 
     await openhabClient.getAllDevices()
         .then(x => _device = x.find(d => d.room === room && d.type == deviceType));
     await openhabClient.getAllRooms()
         .then(x => _room = x.find(r => r.name === room));
 
-    // respond with appropriate sentense if device is not found
     if (!_device)
         conv.close(`Sorry, couldn't find any device of type ${deviceType} in ${_room.description}`);
 
-    // here device is found, get the initial state
     let updatedState: string;
 
     await openhabClient.getItem(_device.id)
         .then(x => _openhabItem = x);
     let initialState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
-    // send command |or| update state
     await openhabClient.sendCommand(_openhabItem.name, (command === undefined || command === "") ? value : command)
         .then(async _ => {
-            // re-fetch updated state  
             await openhabClient.getItem(_device.id).then(x => updatedState = x.state);
         });
-    // close conversation
     conv.close(`${deviceType} in ${_room.description} has benn updated from ${initialState} to ${updatedState}`);
+});
+
+assistant.intent('projecthomecustom.smarthome.zone.state.check', async(conv, { zone, deviceType, value, all }) => {
+    let _devices: Device[];
+    let _zone: Zone;
+    let itemState: string;
+
+    await openhabClient.getAllDevices()
+        .then(x => _devices = x.filter(d => d.zone === zone && d.type == deviceType));
+    await openhabClient.getAllZones()
+        .then(x => _zone = x.find(r => r.name === zone));
+
+    if (!_devices || _devices.length === 0) {
+        conv.close(`Sorry, couldn't find any device of type ${deviceType} in ${_zone.description}`);
+        return;
+    }
+    let responseMessage: string = `Zone: ${zone} for ${deviceType} \n`;
+
+    await Promise.all(_devices.map(async _device => {
+        let _openhabItem: Item;
+
+        await openhabClient.getItem(_device.id)
+            .then(x => {
+                _openhabItem = x;
+
+                itemState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
+                responseMessage = responseMessage.concat(` in ${_device.room} state is ${_openhabItem.state} \n`);
+            });
+    }));
+    conv.ask(new SimpleResponse({
+        text: responseMessage,
+        speech: 'Are you talking to me ?'
+    }));
 });
 
 assistant.intent('projecthomecustom.smarthome.zone.command', async (conv, { zone, deviceType, command, value, all }) => {
     let _devices: Device[];
     let _zone: Zone;
 
-    // fetch configurations
     if (!zone && all === 'true') {
         await openhabClient.getAllDevices()
             .then(x => _devices = x.filter(d => d.type == deviceType));
@@ -143,34 +141,28 @@ assistant.intent('projecthomecustom.smarthome.zone.command', async (conv, { zone
             .then(x => _zone = x.find(r => r.name === zone));
     }
 
-    // respond with appropriate sentense if device list is undefined or empty
     if (!_devices || _devices.length === 0) {
         conv.close(`Sorry, couldn't find any device of type ${deviceType} in ${_zone.description}`);
         return;
     }
-
-    // here, al least one device is found
     let responseMessage: string = `Zone: ${zone}  \n`;
     let updatedState: string;
 
     await Promise.all(_devices.map(async _device => {
         let _openhabItem: Item;
-        // get the initial state
+
         await openhabClient.getItem(_device.id)
             .then(x => _openhabItem = x);
         let initialState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
 
-        // send command |or| update state
         await openhabClient.sendCommand(_openhabItem.name, (command === undefined || command === "") ? value : command)
             .then(async _ => {
-                // re-fetch updated state
                 await openhabClient.getItem(_device.id).then(x => updatedState = x.state);
             });
         responseMessage.concat(`=> ${deviceType} has benn updated from ${initialState} to ${updatedState}  \n`);
         responseMessage = responseMessage.concat(`=> ${deviceType} has benn updated from ${initialState} to ${updatedState}  \n`);
 
     }));
-    // close conversation
     conv.ask(new SimpleResponse({
         text: responseMessage,
         speech: 'Are you talking to me ?'
