688cd714857bbf59121a9ead3749647c5fdc440c
LaureenMartina <L.martina@protonmail.com>
2020-01-30 19:17:21 +0100

    add rich responses (carousel is ok)

--- src/dialogflow-fulfillment/src/index.ts
+++ src/dialogflow-fulfillment/src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
-import {dialogflow, actionssdk, Image, Carousel, BasicCard, Button, SimpleResponse} from 'actions-on-google';
+import {dialogflow, actionssdk, Image, Carousel, BrowseCarouselItem, BasicCard, Button, SimpleResponse, RichResponse} from 'actions-on-google';
 import { OpenhabClient } from './openhabClient'
 import { Item } from './models/openhab/item.model';
 import { Device } from "./models/device.model";
 import { Room } from "./models/room.model";
-import {Zone} from "./models/zone.model";
+import { Zone } from "./models/zone.model";
 
 const openhabClient = new OpenhabClient('https://openhabproxyapi-dev-as.azurewebsites.net', '01c9e186-c1f1-4e94-8bf5-ad86b297d9ba');
 
@@ -15,6 +15,7 @@ const assistant = dialogflow({ debug: false });
 server.set('port', process.env.PORT || 8080);
 server.use(bodyParser.json({ type: 'application/json' }));
 
+//
 assistant.intent('projecthomecustom.smarthome.device.state.check', async (conv, { room, deviceType, all }) => {
     let _device: Device;
     let _devices: Device[];
@@ -43,6 +44,8 @@ assistant.intent('projecthomecustom.smarthome.device.state.check', async (conv, 
         // close conversation
         conv.close(`${deviceType} is ${state} in ${_room.description}`);
     } else {
+        let responseMessage: string = "State : \n";
+
         await openhabClient.getAllDevices()
             .then(x => _devices = x.filter(d => d.type == deviceType));
 
@@ -53,45 +56,67 @@ assistant.intent('projecthomecustom.smarthome.device.state.check', async (conv, 
 
         await Promise.all(_devices.map(async _device => {
             let _openhabItem: Item;
-            let initialState: String;
-            let bidon: String = "OPEN";
-            // get the initial state
+            let itemState: string;
+
             await openhabClient.getItem(_device.id)
                 .then(x => {
                     _openhabItem = x;
-                    initialState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
-                    console.log("Coucouuuuu");
+                    itemState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
+                    responseMessage = responseMessage.concat(`${_device.room} => ${_openhabItem.state} \n`);
                 });
-            //(async _ => {
-            // re-fetch updated state
-            console.log("getItem");
-            await openhabClient.getItem(_device.id).then(x => x.state)
-                .then(resp => {
-                    //x => _devices = x.filter(d => d.type == deviceType);
-                    console.log(`deviceType: ${initialState}`);
-                    responseMessage.concat(`=> ${bidon} has benn updated from ${initialState}  \n`);
-                    //conv.close(`Sorry, couldn't find any device of type ${deviceType}`);
+
+                let _basicCard = new BasicCard({
+                    title: responseMessage,
+                    image: new Image({
+                        url: 'https://goo.gl/Fz9nrQ',
+                        alt: 'Device state check'
+                    })
                 });
-            conv.close(`Sorry, couldn't find any device of type ${deviceType}`);
-            //})
-            ;
-
-            // send command |or| update state
-
-            //responseMessage.concat(`=> ${deviceType} has benn updated from ${initialState} to ${updatedState}  \n`);
-            //responseMessage = responseMessage.concat(`=> ${deviceType} has benn updated from ${initialState} to ${updatedState}  \n`);
         }));
 
-        // conv.close(`${_openhabItem.label} is ${state} in ${_device.room}`);
-        //responseMessage.concat(`=> ${deviceType} has benn updated from ${_state} \n`);
-        //responseMessage = responseMessage.concat(`=> ${deviceType} has benn updated from ${_state}  \n`);
-
-
+        conv.ask(new SimpleResponse({
+            text: responseMessage,
+            speech: 'Are you talking to me ?'
+        }));
     }
 });
 
+// OK
+assistant.intent('projecthomecustom.smarthome.zone.state.check', async(conv, { zone, deviceType, value, all }) => {
+    let _devices: Device[];
+    let _zone: Zone;
+    let itemState: string;
 
+    await openhabClient.getAllDevices()
+        .then(x => _devices = x.filter(d => d.zone === zone && d.type == deviceType));
+    await openhabClient.getAllZones()
+        .then(x => _zone = x.find(r => r.name === zone));
 
+    if (!_devices || _devices.length === 0) {
+        conv.close(`Sorry, couldn't find any device of type ${deviceType} in ${_zone.description}`);
+        return;
+    }
+    let responseMessage: string = `Zone: ${zone} for ${deviceType} \n`;
+
+    await Promise.all(_devices.map(async _device => {
+        let _openhabItem: Item;
+
+        await openhabClient.getItem(_device.id)
+            .then(x => {
+                _openhabItem = x;
+
+                itemState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
+                responseMessage = responseMessage.concat(`in ${_device.room} state is ${_openhabItem.state} \n`);
+            });
+    }));
+
+    conv.ask(new SimpleResponse({
+        text: responseMessage,
+        speech: 'This action is well done !'
+    }));
+});
+
+//OK
 assistant.intent('projecthomecustom.smarthome.device.command', async (conv, { room, deviceType, command, value }) => {
     let _device: Device;
     let _room: Room;
@@ -112,15 +137,50 @@ assistant.intent('projecthomecustom.smarthome.device.command', async (conv, { ro
 
     await openhabClient.getItem(_device.id)
         .then(x => _openhabItem = x);
+
     let initialState = isNaN(+_openhabItem.state) ? _openhabItem.state : (Math.round(+_openhabItem.state * 100) / 100).toFixed(0);
+
     // send command |or| update state
     await openhabClient.sendCommand(_openhabItem.name, (command === undefined || command === "") ? value : command)
         .then(async _ => {
             // re-fetch updated state  
             await openhabClient.getItem(_device.id).then(x => updatedState = x.state);
         });
-    // close conversation
-    conv.close(`${deviceType} in ${_room.description} has benn updated from ${initialState} to ${updatedState}`);
+
+    let _image = new Image({ 
+        url: 'https://goo.gl/Fz9nrQ',
+        alt: 'devide command' 
+    });
+
+    //console.log("image:", _image);
+
+    conv.ask(new Carousel({
+        items: {
+          ["ON"]: {
+            title: `Please ${deviceType} in ${_room.description}`,
+            description: `Manage your House \n Action on ${deviceType} in ${_room.description}`,
+            synonyms: ['activation', 'lightenning']
+          },
+          ["OFF"]: {
+            title: `Mmmm...${deviceType} in ${_room.description}`,
+            description: `Manage your House \n Deactivation of on ${deviceType} in ${_room.description}`,
+            synonyms: ['deactivation', 'shut off'],
+          }
+        }
+    }));
+
+    //if (conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT') && conv.surface.capabilities.has('actions.capability.WEB_BROWSER')) {
+        console.log("command:", command);
+        if (command == "ON") {
+            console.log("command:", command);
+            conv.close(new SimpleResponse({ 
+                text: `${deviceType} in ${_room.description} has been updated from ${initialState} to ${updatedState}`,
+                speech: `${deviceType} in ${_room.description} has been updated from ${initialState} to ${updatedState}`
+            }));
+        } else {
+            conv.close(`${deviceType} in ${_room.description} has been updated`);
+        }
+    //}
 });
 
 assistant.intent('projecthomecustom.smarthome.zone.command', async (conv, { zone, deviceType, command, value, all }) => {
@@ -151,7 +211,9 @@ assistant.intent('projecthomecustom.smarthome.zone.command', async (conv, { zone
 
     // here, al least one device is found
     let responseMessage: string = `Zone: ${zone}  \n`;
+
     let updatedState: string;
+    let _richResponse;
 
     await Promise.all(_devices.map(async _device => {
         let _openhabItem: Item;
@@ -166,15 +228,32 @@ assistant.intent('projecthomecustom.smarthome.zone.command', async (conv, { zone
                 // re-fetch updated state
                 await openhabClient.getItem(_device.id).then(x => updatedState = x.state);
             });
-        responseMessage.concat(`=> ${deviceType} has benn updated from ${initialState} to ${updatedState}  \n`);
-        responseMessage = responseMessage.concat(`=> ${deviceType} has benn updated from ${initialState} to ${updatedState}  \n`);
+        responseMessage.concat(`=> ${deviceType} has been updated from ${initialState} to ${updatedState}  \n`);
+        responseMessage = responseMessage.concat(`=> ${deviceType} has been updated from ${initialState} to ${updatedState}  \n`);
+
+        _richResponse = new BasicCard({
+                title: responseMessage,
+                image: new Image({ 
+                    url: 'https://image.noelshack.com/fichiers/2020/05/4/1580397491-pouet.jpg',
+                    alt: 'Maison Logo' 
+                }),
+                buttons: new Button({
+                    title: 'Watch',
+                    url: 'https://angularfirebase.com/lessons',
+                })
+        });
 
     }));
     // close conversation
-    conv.ask(new SimpleResponse({
-        text: responseMessage,
-        speech: 'Are you talking to me ?'
-    }));
+    //if (conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT') && conv.surface.capabilities.has('actions.capability.WEB_BROWSER')) {
+        conv.ask('here is what I found little human ...')
+        conv.close(_richResponse);
+    //}else{
+        conv.close(new SimpleResponse({ 
+            text: responseMessage,
+            speech: `Are you talking to me ?`,
+        }));
+    //}
 });
 
 server.post('/webhook', assistant);
